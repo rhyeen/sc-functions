679874e70ea65f2ef339e9f9ae47baf9d3fef595
Ryan Saunders <>
2019-11-17 20:27:21 -0700

    test passing

--- functions/src/routes/admin.test.ts
+++ functions/src/routes/admin.test.ts
@@ -4,12 +4,14 @@ import firebaseFunctionsTest from 'firebase-functions-test';
 // @NOTE: need to hit the actual index since admin.initializeApp() needs to be called.
 import * as myFunctions from '../index';
 import { firestoreDB } from '../services/firestore';
-let adminInitStub, dbCollectionStub;
+let adminInitStub, dbCollectionStub, docsStub, addStub;
 
 
 beforeEach(() => {
   adminInitStub = sinon.stub(admin, 'initializeApp');
   dbCollectionStub = sinon.stub(firestoreDB, 'collection');
+  docsStub = sinon.stub();
+  addStub= sinon.stub();
 });
 
 afterEach(() => {
@@ -18,26 +20,57 @@ afterEach(() => {
   firebaseFunctionsTest().cleanup();
 });
 
+function firestoreGetStub(collection: string, doc: string, getSnapshotData:any):void {
+  dbCollectionStub.withArgs(collection).returns({
+    doc: docsStub
+  });
+  const snapshot = firebaseFunctionsTest().firestore.makeDocumentSnapshot(getSnapshotData, `${collection}/${doc}`);
+  docsStub.withArgs(doc).returns({
+    get: () => new Promise((resolve) => resolve(snapshot))
+  });
+}
+
+function firestoreAddStub(collection: string, data:any, refId:string):void {
+  dbCollectionStub.withArgs(collection).returns({
+    add: addStub
+  });
+  addStub.withArgs(data).returns({
+    id: refId
+  });
+}
+
 describe('generateSeed', () => {
   test('no input', () => {
-    dbCollectionStub.withArgs('dungeonconditions').returns({
-      doc: (_) => { 
-        return {
-          get: () => {
-            return new Promise((resolve) => {
-              resolve(firebaseFunctionsTest().firestore.makeDocumentSnapshot({
-                cards: {
-                  'CD_1': { weight: 1 },
-                  'CD_2': { weight: 2 },
-                  'CD_3': { weight: 3 },
-                },
-                name: 'default test'
-              }, 'dungeonconditions/test'));
-            });
-          }
-        };
+    const dungeonCondition = {
+      cards: {
+        'CD_1': { weight: 1 },
+        'CD_2': { weight: 2 },
+        'CD_3': { weight: 3 },
+      },
+      name: 'default test'
+    };
+    firestoreGetStub('dungeonconditions', 'test', dungeonCondition);
+    const cards = [
+      {
+        id: 'CD_1',
+        name: 'test card 1'
+      },
+      {
+        id: 'CD_2',
+        name: 'test card 2'
+      },
+      {
+        id: 'CD_3',
+        name: 'test card 3'
       }
-    });
+    ];
+    for (const card of cards) {
+      firestoreGetStub('dungeoncards', card.id, card);
+    }
+    firestoreAddStub('dungeonseeds', {
+      ...dungeonCondition,
+      dungeoncards: [...cards]
+    }, '123');
     const req = { headers: { origins: true }, body: { dungeonId: 'test' } };
     const res = {
       setHeader: () => {},
@@ -45,7 +78,7 @@ describe('generateSeed', () => {
       status: (code: number) => {
         expect(code).toEqual(200);
         return { json: (body: any) => {
-          expect(body).toEqual({ data: { seed: 'TEST' }});
+          expect(body).toEqual({ data: { seed: '123' }});
         }};
       }
     };
